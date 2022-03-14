e1c098ca0b9e1213bbc4b4a6211a55760de03074
Maxime Caignart <maxime@caignart.fr>
2022-03-14 13:55:04 +0100

    adding logs to dll loader

--- MonoGame.Framework/Platform/Utilities/FuncLoader.Desktop.cs
+++ MonoGame.Framework/Platform/Utilities/FuncLoader.Desktop.cs
@@ -32,46 +32,79 @@ namespace MonoGame.Framework.Utilities
             [DllImport("/usr/lib/libSystem.dylib")]
             public static extern IntPtr dlsym(IntPtr handle, string symbol);
         }
-        
+
         private const int RTLD_LAZY = 0x0001;
 
         public static IntPtr LoadLibraryExt(string libname)
         {
-            var ret = IntPtr.Zero;
+            Console.Out.WriteLine("Loading ext lib : {0}", libname);
+            IntPtr ret;
             var assemblyLocation = Path.GetDirectoryName(typeof(FuncLoader).Assembly.Location) ?? "./";
+            Console.Out.WriteLine("- assembly location  : {0}", assemblyLocation);
+            Console.Out.WriteLine(">>> full Path  : {0}", Path.GetFullPath(assemblyLocation));
 
+            Console.Out.WriteLine("- Try .NET Framework / mono locations");
             // Try .NET Framework / mono locations
             if (CurrentPlatform.OS == OS.MacOSX)
             {
-                ret = LoadLibrary(Path.Combine(assemblyLocation, libname));
-
+                Console.Out.WriteLine("- CurrentPlatform.OS == OS.MacOSX");
+                ret = LoadLibraryExtWithLogs(Path.Combine(assemblyLocation, libname));
                 // Look in Frameworks for .app bundles
                 if (ret == IntPtr.Zero)
-                    ret = LoadLibrary(Path.Combine(assemblyLocation, "..", "Frameworks", libname));
+                {
+                    Console.Out.WriteLine("- Look in Frameworks for .app bundles");
+                    ret = LoadLibraryExtWithLogs(Path.Combine(assemblyLocation, "..", "Frameworks", libname));
+                }
             }
             else
             {
+                Console.Out.WriteLine("- CurrentPlatform.OS != OS.MacOSX");
                 if (Environment.Is64BitProcess)
-                    ret = LoadLibrary(Path.Combine(assemblyLocation, "x64", libname));
+                {
+                    Console.Out.WriteLine("- Env is 64bit");
+                    ret = LoadLibraryExtWithLogs(Path.Combine(assemblyLocation, "x64", libname));
+                }
                 else
-                    ret = LoadLibrary(Path.Combine(assemblyLocation, "x86", libname));
+                {
+                    Console.Out.WriteLine("- Env is 32bit");
+                    ret = LoadLibraryExtWithLogs(Path.Combine(assemblyLocation, "x86", libname));
+                }
             }
+
 
             // Try .NET Core development locations
             if (ret == IntPtr.Zero)
-                ret = LoadLibrary(Path.Combine(assemblyLocation, "runtimes", CurrentPlatform.Rid, "native", libname));
+            {
+                Console.Out.WriteLine("- Try .NET Core development locations");
+                ret = LoadLibraryExtWithLogs(Path.Combine(assemblyLocation, "runtimes", CurrentPlatform.Rid, "native", libname));
+            }
 
             // Try current folder (.NET Core will copy it there after publish)
             if (ret == IntPtr.Zero)
-                ret = LoadLibrary(Path.Combine(assemblyLocation, libname));
+            {
+                Console.Out.WriteLine("-  Try current folder (.NET Core will copy it there after publish)");
+                ret = LoadLibraryExtWithLogs(Path.Combine(assemblyLocation, libname));
+            }
 
             // Try loading system library
             if (ret == IntPtr.Zero)
-                ret = LoadLibrary(libname);
+            {
+                Console.Out.WriteLine("- Try loading system library");
+                ret = LoadLibraryExtWithLogs(libname);
+            }
 
             // Welp, all failed, PANIC!!!
             if (ret == IntPtr.Zero)
                 throw new Exception("Failed to load library: " + libname);
+
+            return ret;
+        }
+
+        private static IntPtr LoadLibraryExtWithLogs(string path)
+        {
+            IntPtr ret = LoadLibrary(path);
+            Console.Out.WriteLine("- LoadLibrary at {0} : result={1}", path, ret);
+            Console.Out.WriteLine(">>> Exists {1} at full Path  : {0}", path, File.Exists(path));
 
             return ret;
         }
